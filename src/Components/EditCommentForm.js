2b855d80fb66ac80eede46629263b5f25e4fb448
Milene <badouxmilene@gmail.com>
2017-09-14 18:44:22 -0700

    Add form-redux

--- src/Components/EditCommentForm.js
+++ src/Components/EditCommentForm.js
@@ -6,31 +6,62 @@ import FloatingActionButton from 'material-ui/FloatingActionButton'
 import TextField from 'material-ui/TextField'
 import Back from 'mui-icons/fontawesome/arrow-left'
 import RaisedButton from 'material-ui/RaisedButton'
+import { Field, reduxForm } from 'redux-form'
+
+const required = value => (value ? undefined : 'Required')
+
+
+const getBody = (comments,id) => {
+  if(comments && id){
+    let bodyInit = comments.find(item =>item.id === id)
+    if (bodyInit){
+      return(bodyInit.body)
+    }
+  }
+}
+
+const renderTextFieldBody = ({ body, input, label, meta: { touched, error } }) => (
+    <TextField
+      hintText={label}
+      multiLine={true}
+      fullWidth={true}
+      floatingLabelText={label}
+      errorText={touched && error}
+      {...input}
+  />
+  )
 
 class EditCommentForm extends React.Component {
-
+  
 
 render () {
   const { comments, updateComment, history } = this.props
-
   return (
     <div>
       <FloatingActionButton mini children={<Back/>} onClick={() => {
       history.goBack()
     }}/>
       {comments && (
+
         <div className='center'>
           <h2 className='row text-align-center'>Edit a Comment</h2>
-        <label  htmlFor="body"><b>Body</b></label>
-        <TextField multiLine={true}  name='bodyInput' ref='bodyInput' defaultValue={comments.body} fullWidth={true}/>
-      <RaisedButton primary type="submit" label='submit' onClick={()=> {
-        let input = {
-          id: comments.id,
-          parentId: comments.parentId,
-          body: this.refs.bodyInput.input.refs.input.value
-        };
-        updateComment(input);
-      }}/>
+          <form >
+            <div>
+            <Field
+              body={comments.body}
+              validate={required}
+              ref='bodyInput' name="body" component={renderTextFieldBody} label="Body" />
+            </div>
+          </form>
+          <RaisedButton primary type="submit" label='submit' onClick={()=> {
+          let input = {
+            id: comments.id,
+            parentId: comments.parentId,
+            body: this.refs.bodyInput.value
+          };
+          updateComment(input);
+        }}/>
+
     </div>
     )}
   </div>
@@ -39,14 +70,28 @@ render () {
 }
 }
 
+
 // Map state to props
 const mapStateToProps = (state, props) => {
-  if(props.match.params.comment_id) {
+  let body = getBody(state.comments, props.match.params.comment_id)
+
+  if(props.match.params.comment_id && body) {
     return {
-      comments: state.comments.find(item =>item.id === props.match.params.comment_id)
+      comments: state.comments.find(item =>item.id === props.match.params.comment_id),
+      initialValues: {body}
     }
   }
+  else {
+    return {
+     comments: state.comments.find(item =>item.id === props.match.params.comment_id),
+   }
+  }
 }
+
+EditCommentForm = reduxForm({
+  form: 'editCommentForm'
+})(EditCommentForm)
+
 // Map dispatch to props
 const mapDispatchToProps = (dispatch) => {
     return {
